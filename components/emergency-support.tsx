0bd927c75fbc84c137b20d95be26b1d478fc8826
Kokonutman <arjun.iyer42@gmail.com>
2025-04-13 09:22:59 -0400

    updated number again

--- components/emergency-support.tsx
+++ components/emergency-support.tsx
@@ -49,7 +49,7 @@ export function EmergencySupport() {
               className="w-full md:w-auto px-8 bg-accent hover:bg-accent/90"
             >
               <Phone className="mr-2 h-5 w-5" />
-              +1 (947) 217 5415
+              +1 (855) 608 9948
             </Button>
 
             <ul className="space-y-2 text-muted-foreground">
